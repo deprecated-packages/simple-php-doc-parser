7e2dfcb880fbc4f780ab9f1d6afb27e8f30f5499
Tomas Votruba <tomas.vot@gmail.com>
2020-10-11 12:40:45 +0200

    [SimplePhpDocParser] Init (#4391)

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPStan\PhpDocParser\Lexer\Lexer;
+use PHPStan\PhpDocParser\Parser\PhpDocParser;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Rector\SimplePhpDocParser\\', __DIR__ . '/../src');
+
+    $services->set(PhpDocParser::class);
+    $services->set(Lexer::class);
+};
